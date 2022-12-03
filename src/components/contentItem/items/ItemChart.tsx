dd6f950cecca750a25fd372087456f1ea6b84bd2
Max <max11cz@seznam.cz>
2022-12-03 20:26:23 +0100

    gg

--- src/components/contentItem/items/ItemChart.tsx
+++ src/components/contentItem/items/ItemChart.tsx
@@ -3,46 +3,39 @@ export const ItemChart = (props: any) => {
 
     const data = [
         {
-            name: 'Page A',
+            name: 'A',
             uv: 4000,
             pv: 2400,
-            amt: 2400,
         },
         {
-            name: 'Page B',
+            name: 'B',
             uv: 3000,
             pv: 1398,
-            amt: 2210,
         },
         {
-            name: 'Page C',
+            name: 'C',
             uv: 2000,
             pv: 9800,
-            amt: 2290,
         },
         {
-            name: 'Page D',
+            name: 'D',
             uv: 2780,
             pv: 3908,
-            amt: 2000,
         },
         {
-            name: 'Page E',
+            name: 'E',
             uv: 1890,
-            pv: 10000,
-            amt: 2181,
+            pv: 10000, 
         },
         {
-            name: 'Page F',
+            name: 'F',
             uv: 2390,
             pv: 3800,
-            amt: 2500,
         },
         {
             name: 'Page G',
             uv: 3490,
             pv: 4300,
-            amt: 2100,
         },
     ];
     return (<div className='flex pt-6 justify-center'>
